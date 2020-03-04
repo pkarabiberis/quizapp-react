a38959a01e9aa160c570ab8e21c91f87d5030cf5
cahvi <36645760+cahvi@users.noreply.github.com>
2020-03-04 11:46:55 +0200

    init the app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { QuizList } from './components/QuizList';
+
+export const App = () => {
+	return (
+		<div className="App">
+			<QuizList />
+		</div>
+	);
+};
